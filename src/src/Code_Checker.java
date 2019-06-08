0d2a481a5a5a9a5f325c37725d98bb9c1e05b956
prabhatverma286 <prabhatverma286@gmail.com>
2019-06-08 11:35:56 +0100

    Basic code checker added

---
+++ src/src/Code_Checker.java
@@ -0,0 +1,16 @@
+public class Code_Checker {
+
+    public boolean check(Colours[] code, Colours[] guess)
+    {
+        if(code.length != guess.length)
+            return false;
+
+        for(int i = 0; i < code.length; i++)
+        {
+            if(code[i] != guess[i])
+                return false;
+        }
+
+        return true;
+    }
+}
